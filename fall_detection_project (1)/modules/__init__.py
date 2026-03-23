95259c029fce59cc95f68acd6b844f1e597a4262
CYanggggg <166628778+CYanggggg@users.noreply.github.com>
2026-03-23 14:54:27 +0800

    Delete fall_detection_project (1) directory

--- fall_detection_project (1)/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from .detector import PersonDetector, Detection
-from .tracker import ByteTracker, Track
-from .pose_extractor import PoseExtractor
-from .visualiser import Visualiser
-from .logger import DataLogger
-try:
-    from .fall_detector import FallDetector, FallLSTM
-except ImportError:
-    FallDetector = FallLSTM = None
